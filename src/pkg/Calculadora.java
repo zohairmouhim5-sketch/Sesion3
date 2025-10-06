86ee6ec71c91ec24e0afa01eee9f1ddcd980cc75
zmg <zohair.mouhim@alu.uhu.es>
2025-10-06 17:19:32 +0200

    3 commit practica 3

--- src/pkg/Calculadora.java
+++ src/pkg/Calculadora.java
@@ -1,25 +1,29 @@
 package pkg;
-
+/*
+ * primer paso hacer los test sin codigo
+ * segundo poner return y el resultado esperaddo 
+ * tercero probarlo bien hecho
+ */
 public class Calculadora {
 
 	public static Integer suma(int i, int j) {
 		
-		return 5;
+		return i+j;
 	}
 	
 	public static Integer restar(int i, int j) {
 		
-		return 3;
+		return i-j;
 	}
 	
 	public static Integer multiplicar(int i, int j) {
 		
-		return 20;
+		return i*j;
 	}
 	
 	public static Integer dividir(int i, int j) {
 		
-		return 0;
+		return i/j;
 	}
 
 }
